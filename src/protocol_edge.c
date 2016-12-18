dechamps/tinc

commit e4670fc4a0576eb76f1807ce29fa9455dd247632

Improve performance of edge updates.

src/protocol_edge.c
@@ -125,8 +125,19 @@ bool add_edge_h(connection_t *c) {
 			} else {
 				ifdebug(PROTOCOL) logger(LOG_WARNING, "Got %s from %s (%s) which does not match existing entry",
 						   "ADD_EDGE", c->name, c->hostname);
-				edge_del(e);
+				e->options = options;
+				if(sockaddrcmp(&e->address, &address)) {
+					sockaddrfree(&e->address);
+					e->address = address;
+				}
+				if(e->weight != weight) {
+					avl_node_t *node = avl_unlink(edge_weight_tree, e);
+					e->weight = weight;
+					avl_insert_node(edge_weight_tree, node);
+				}
+
 				graph();
+				return true;
 			}
 		} else
 			return true;

commit d0b1c2dae67bce3f3a81ef43f22a1ef27d1f0ade

Fix edge updates containing local address changes.
This commit fixes a logic bug in the edge update code where local
address changes are not taken into account if they are bundled in with
other changes. This bug breaks local discovery in some scenarios.

The regression was introduced by commit
e4670fc4a0576eb76f1807ce29fa9455dd247632.

src/protocol_edge.c
@@ -132,7 +132,13 @@ bool add_edge_h(connection_t *c, const char *request) {
 	e = lookup_edge(from, to);
 
 	if(e) {
-		if(e->weight != weight || e->options != options || sockaddrcmp(&e->address, &address)) {
+		// local_address.sa.sa_family will be 0 if we got it from older tinc versions
+		// local_address.sa.sa_family will be 255 (AF_UNKNOWN) if we got it from newer versions
+		// but for edge which does not have local_address
+		bool new_local_address = local_address.sa.sa_family && local_address.sa.sa_family != AF_UNKNOWN &&
+			sockaddrcmp(&e->local_address, &local_address);
+
+		if(e->weight != weight || e->options != options || sockaddrcmp(&e->address, &address) || new_local_address) {
 			if(from == myself) {
 				logger(DEBUG_PROTOCOL, LOG_WARNING, "Got %s from %s (%s) for ourself which does not match existing entry",
 						   "ADD_EDGE", c->name, c->hostname);
@@ -147,6 +153,10 @@ bool add_edge_h(connection_t *c, const char *request) {
 					sockaddrfree(&e->address);
 					e->address = address;
 				}
+				if(new_local_address) {
+					sockaddrfree(&e->local_address);
+					e->local_address = local_address;
+				}
 				if(e->weight != weight) {
 					splay_node_t *node = splay_unlink(edge_weight_tree, e);
 					e->weight = weight;
@@ -154,36 +164,6 @@ bool add_edge_h(connection_t *c, const char *request) {
 				}
 
 				goto done;
-			}
-		} else if(sockaddrcmp(&e->local_address, &local_address)) {
-			if(from == myself) {
-				if(e->local_address.sa.sa_family && local_address.sa.sa_family) {
-					// Someone has the wrong local address for ourself. Correct then.
-					logger(DEBUG_PROTOCOL, LOG_WARNING, "Got %s from %s (%s) for ourself which does not match existing entry",
-							   "ADD_EDGE", c->name, c->hostname);
-					send_add_edge(c, e);
-					sockaddrfree(&local_address);
-					return true;
-				}
-				// Otherwise, just ignore it.
-				sockaddrfree(&local_address);
-				return true;
-			} else if(local_address.sa.sa_family && local_address.sa.sa_family != AF_UNKNOWN) {
-				// We learned a new local address for this edge.
-				// local_address.sa.sa_family will be 0 if we got it from older tinc versions
-				// local_address.sa.sa_family will be 255 (AF_UNKNOWN) if we got it from newer versions
-				// but for edge which does not have local_address
-				sockaddrfree(&e->local_address);
-				e->local_address = local_address;
-
-				// Tell others about it.
-				if(!tunnelserver)
-					forward_request(c, request);
-
-				return true;
-			} else {
-				sockaddrfree(&local_address);
-				return true;
 			}
 		} else {
 			sockaddrfree(&local_address);